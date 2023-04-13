8b7a8596e22d02ae1b5d6aed7343242c246cfd5f
vats0914 <72242500+vats0914@users.noreply.github.com>
2023-04-14 01:34:54 +0530

    Initial Commit
    Added Card class, Suit  and Rank enums.

---
+++ src/main/java/org/example/Card.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Card {
+    private Suit suit;
+    private Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
---
+++ src/main/java/org/example/Suit.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public enum Suit {
+    CLUBS, DIAMONDS, HEARTS, SPADES;
+
+}
